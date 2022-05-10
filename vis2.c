a41c6a37f550006beb17b799720279e3d3e01de4
HunterPlus <zlw08140957@gmail.com>
2022-05-10 08:11:53 +0800

    Update vis2.c

--- vis2.c
+++ vis2.c
@@ -5,4 +5,14 @@
 
 int main(int argc, char *argv[])
 {
+	int	c, strip = 0;
+	
+	if (argc > 1 && strcmp(argv[1], "-s") == 0)
+		strip = 1;
+	while ((c = getchar()) != EOF)
+		if (isascii(c) && (isprint(c) || c == '\n' || c == '\t' || c == ' '))
+			putchar(c);
+		else if (!strip)
+			printf("\\%03o", c);
+	return 0;
 }
