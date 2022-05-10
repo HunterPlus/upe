4ecc98ffd54e97aa5e182ea0e6dc3513dbccc5f3
HunterPlus <zlw08140957@gmail.com>
2022-05-10 14:11:33 +0800

    Update p.c

--- p.c
+++ p.c
@@ -5,12 +5,13 @@
 #define PAGESIZE	22
 
 void print(FILE *, int);
+FILE *efopen(char *, char *);	/* fopen file, die if can't */
 
 char	*progname;	/* program name for error message */
 int main(int argc, char *argv[])
 {
 	int	i;
-	FILE	*fp, *efopen();
+	FILE	*fp;
 	
 	progname = argv[0];
 	if (argc == 1)
@@ -22,4 +23,14 @@ int main(int argc, char *argv[])
 			fclose(fp);
 		}
 	exit(0);
+}
+
+FILE *efopen(char *file, char *mode)
+{
+	FILE *fp;
+	
+	if ((fp = fopen(file, mode)) != NULL)
+		return fp;
+	fprintf(stderr, "%s: can't open file %s mode %s\n", progname, file, mode);
+	exit(1);
 }
