c8f871071ac16ce04e4d099cc41c2915b4e22733
HunterPlus <zlw08140957@gmail.com>
2022-05-10 14:05:38 +0800

    Update p.c

--- p.c
+++ p.c
@@ -1,10 +1,25 @@
 /* p: print input in chunks (version 1)
 
 #include <stdio.h>
+#include <stdlib.h>
 #define PAGESIZE	22
-char	*progname;	/* program name for error message */
 
-int main()
+void print(FILE *, int);
+
+char	*progname;	/* program name for error message */
+int main(int argc, char *argv[])
 {
+	int	i;
+	FILE	*fp, *efopen();
 	
+	progname = argv[0];
+	if (argc == 1)
+		print(stdin, PAGESIZE);
+	else
+		for (i = i; i < argc; i++) {
+			fp = efopen(argv[i], "r");
+			print(fp, PAGESIZE);
+			fclose(fp);
+		}
+	exit(0);
 }
