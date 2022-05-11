accc8d8f4d800ded4805ce10f3b492d4aaddbf33
HunterPlus <zlw08140957@gmail.com>
2022-05-11 10:28:44 +0800

    Update pick.c

--- pick.c
+++ pick.c
@@ -2,9 +2,16 @@
 
 #include <stdio.h>
 
-void pick(char *);
+void pick(char *);		/* offer choice of string s	*/
 
 int main(int argc, char *argv[])
 {
 	
+}
+
+void pick(char *s)
+{
+	fprintf(stderr, "%s? ", s);
+	if (ttyin() == 'y')
+		printf("%s\n", s);
 }
