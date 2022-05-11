4d830439a780f0059b741b0faae54612e4d0514a
HunterPlus <zlw08140957@gmail.com>
2022-05-11 13:25:56 +0800

    Create zap.c

---
+++ zap.c
@@ -0,0 +1,12 @@
+/* zap: interactive process killer	*/
+
+#include <stdio.h>
+#include <signal.h>
+
+char 	*progname;		/* program name for error message	*/
+char 	*ps = "ps -ag";		/* system dependent	*/
+
+int main(int argc, char *argv[])
+{
+	
+}
