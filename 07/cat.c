ce3619c07b5f48239415e08dd2ddbf440c9e4acd
HunterPlus <zlw08140957@gmail.com>
2022-05-12 14:43:49 +0800

    Create cat.c

---
+++ 07/cat.c
@@ -0,0 +1,15 @@
+/* cat: minimal version */
+
+#include <stdlib.h>
+#include <unistd.h>
+#define SIZE	512	/* arbitrary	*/
+
+int main(int argc, char *argv[])
+{
+	char	buf[SIZE];
+	int	n;
+	
+	while ((n = read(0, buf, sizeof buf)) > 0)
+		write(1, buf, n);
+	exit(0);
+}
