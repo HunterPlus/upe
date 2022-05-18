79e345dcce50ee8162fdce55c1b2409df4c896b1
HunterPlus <zlw08140957@gmail.com>
2022-05-18 14:02:02 +0800

    Create hoc.h

---
+++ 08/hoc4/hoc.h
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <errno.h>
+
+typedef struct Symbol { 		/* symbol table entry */
+	char	*name;
+	short	type;			/* VAR, BLTIN, UNDEF */
+	union {
+		double	val;		/* if VAR */
+		double	(*ptr)();	/* if BLTIN */
+	} u;
+	struct Symbol	*next; 		/* to link to another */
+} Symbol;
+
+int yylex(void);
+void yyerror(char *);
+void execerror(char *, char*);
+void init(void);
+
+Symbol *install(char *, int, double);
+Symbol *lookup(char *);
+
+void *emalloc(unsigned int);
+
+double Log(double);
+double Log10(double);
+double Exp(double);
+double Sqrt(double);
+double integer(double);
+double Pow(double, double);
+double errcheck(double, char *);
